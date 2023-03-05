72537474c6d22eb20d138dd65920446a041833e5
Deepak Yadav <56887879+deepakyadav810@users.noreply.github.com>
2023-03-05 16:13:17 +0530

    Add files via upload

---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+type User struct {
+	Name      string   `json:"name" bson:"user_name"`
+	Age       int      `json:"age" bson:"user_age"`
+	Gender    string   `json:"gender" bson:"user_gender"`
+	Pin       int      `json:"pin" bson:"user_pin"`
+	AccountNo int      `json:"no" bson:"user_no"`
+	Balance   string   `json:"balance" bson:"user_balance"`
+	Statement []string `json:"statement" bson:"user_statement"`
+}
