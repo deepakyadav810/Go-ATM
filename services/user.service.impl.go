461b965bc37afd543ddaa1af152cb71da8c130f1
Deepak Yadav <56887879+deepakyadav810@users.noreply.github.com>
2023-03-07 15:08:26 +0530

    Update user.service.impl.go

--- services/user.service.impl.go
+++ services/user.service.impl.go
@@ -2,6 +2,9 @@ package services
 
 import (
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -14,6 +17,48 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	//"golang.org/x/text/number"
 )
+
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+
+// This should be in an env file in production
+const MySecret string = "abc&1*~#^2^#s0^=)^^7!b34"
+
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// Encrypt method is to encrypt or hide any classified text
+func Encrypt(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return Encode(cipherText), nil
+}
+
+// Decrypt method is to extract back the encrypted text
+func Decrypt(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText), nil
+}
 
 type UserServiceImpl struct {
 	usercollection *mongo.Collection
@@ -59,6 +104,12 @@ func stringaddsub(x1, x2 string) string {
 func (u *UserServiceImpl) CreateUser(user *models.User) (int, error) {
 	user.AccountNo = generate_accno(100000000, 999999999)
 	user.Balance = "0"
+	encText, er := Encrypt(user.Pin, MySecret)
+	fmt.Println("encText", encText)
+	user.Pin = encText
+	if er != nil {
+		fmt.Println("error encrypting your classified text: ", er)
+	}
 	_, err := u.usercollection.InsertOne(u.ctx, user)
 	return user.AccountNo, err
 }
@@ -81,7 +132,11 @@ func (u *UserServiceImpl) DepositWithdraw(user []string) error {
 
 func (u *UserServiceImpl) ChangePin(user []string) error {
 	filter := bson.D{bson.E{Key: "user_no", Value: stringtoint(user[0])}}
-	update := bson.D{bson.E{Key: "$set", Value: bson.D{bson.E{Key: "user_pin", Value: stringtoint(user[1])}}}}
+	encText, er := Encrypt(user[1], MySecret)
+	if er != nil {
+		fmt.Println("error encrypting your classified text: ", er)
+	}
+	update := bson.D{bson.E{Key: "$set", Value: bson.D{bson.E{Key: "user_pin", Value: stringtoint(encText)}}}}
 	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
 	if result.MatchedCount != 1 {
 		return errors.New("No matched Document found for update")
